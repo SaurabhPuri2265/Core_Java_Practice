d752a43420499d4a9f51af7caba5cc6f1d40c9e1
saurabhpuri <SaurabhPuri2265>
2021-01-14 14:20:41 +0530

    Started Core Java Practice

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        //variable names should start with an alphabet only
+
+        int num=5;
+
+        String word="Hello";
+
+        char c='A';
+
+        long ln= 45678443;
+
+        double dbl=345.765;
+
+        System.out.println(num+10); // + is an arithmetic operator
+
+        System.out.println(word+12); // + is an concatenation operator
+
+        System.out.println(c+'A'); // + adds the ASCII values of c variable and 'A'
+
+    }
+}
